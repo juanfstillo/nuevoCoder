9045cae33c56faf987a0dd88248d9b635d20974b
juanfstillo <juan.stillo1@gmail.com>
2022-07-12 15:25:40 -0300

    entrega optativa

---
+++ entregas/entregaoptativa/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+      <div>
+          <Tabla :titulos="tabla1.titulos" :registros="tabla1.registros" :clase="tabla1.clase"/>
+      </div>   
+      <div>
+          <Tabla :titulos="tabla2.titulos" :registros="tabla2.registros" :clase="tabla2.clase"/>
+      </div>
+      <div>
+          <Tabla :titulos="tabla3.titulos" :registros="tabla3.registros" :clase="tabla3.clase"/>
+      </div>
+  </div>
+</template>
+
+<script>
+import Tabla from './components/Tabla.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Tabla
+  },
+  data(){
+    return {
+      tabla1: {
+            titulos: ["ID", "Nombre", "Director", "Año"],
+            registros: [
+                ["La Mascara", "Velez", "1999"],
+                ["El Rey Leon", "Gomez", "2001 "],
+                ["Titanic", "Godino", "2001"],
+                ["Los Simpsons", "Gimenez", "1998"],
+            ],
+            clase:"azul fondoRojo",
+        },
+        tabla2: {
+            titulos: ["ID", "Linea", "Ramal", "Color"],
+            registros: [
+                ["93", "Avellaneda", "Amarillo"],
+                ["107", "Ciudad Universitaria", "Blanco"],
+                ["111", "Constitucion", "Verde"],
+                ["10", "Wilde", "Cyan"],
+            ],
+            clase:"rojo fondoAzul",
+        },
+        tabla3: {
+            titulos: ["ID", "Gerencia", "Origen", "Destino"],
+            registros: [
+                ["Roca", "Constitucion", "Retiro"],
+                ["San Martin", "Retiro", "Pilar"],
+                ["Servicio Nacional", "A", "B"],
+                ["Tren de la Costa", "Retiro", "Olivos"],
+            ],
+            clase:"verde fondoGris"
+        },
+    }
+  }
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ entregas/entregaoptativa/src/components/Tabla.vue
@@ -0,0 +1,62 @@
+<template>
+        <div>
+            <table class="table table-striped table-dark">
+                <thead class="thead-light">
+                    <tr>
+                        <th v-for="(item, i) in titulos" :key="i" scope="col">{{ item }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(registro, i) in registros" :key="i">
+                        <th :class="clase">{{ i + 1 }}</th>
+                        <th :class="clase" v-for="(row, r) in registro" :key="r">{{row}}</th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+</template>
+
+<script>
+export default {
+  name: 'Tabla',
+  props: {
+      titulos:{
+        type:Array,
+        required:true
+      },
+      registros:{
+        type: Array,
+        required:true,
+      },
+      clase:{
+        type:String,
+        required:true,
+
+      }
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.azul{
+    color:blue;
+}
+.rojo{
+    color:red;
+}
+.verde{
+    color: green
+}
+.fondoAzul{
+    background-color: blue;
+}
+
+.fondoRojo{
+    background-color: red;
+}
+
+.fondoGris{
+    background-color: grey;
+}
+</style>
