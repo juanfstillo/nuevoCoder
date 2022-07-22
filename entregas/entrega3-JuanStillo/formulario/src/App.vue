59901f9933f5c217fbd8069428692a18c659ada4
juanfstillo <juan.stillo1@gmail.com>
2022-07-22 09:57:46 -0300

    entrega3

---
+++ entregas/entrega3-JuanStillo/formulario/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Formulario Clase 8"/>
+    <Formulario @emit-agregar-usuario="addUsuarioTabla"/>
+    <Tabla :usuarios="usuarios"/>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Formulario from './components/Fomulario.vue'
+import Tabla from './components/Tabla.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld, Formulario,Tabla
+  },
+  data() {
+        return {
+            usuarios: []
+        }
+    },
+    methods: {
+        addUsuarioTabla(usuario) {
+            console.log(usuario);
+            const nuevoUsuario = {...usuario};
+            this.usuarios.push(nuevoUsuario);
+        }
+    }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ entregas/entrega3-JuanStillo/formulario/src/components/Tabla.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container" v-if="usuarios.length != 0">
+        <table class="table table-striped table-dark">
+            <thead class="thead-light">
+                 <tr>
+                     <th>#</th>
+                    <th>Nombre</th>
+                    <th>Edad</th>
+                    <th>Email</th>
+                 </tr>
+            </thead>
+            <tbody>
+                 <tr v-for="(item, i) in usuarios" :key="item.id">
+                    <td>{{i+1 | capitalize}}</td>
+                    <td>{{item.nombre | capitalize}}</td>
+                    <td>{{item.edad}}</td>
+                    <td>{{item.email | capitalize}}</td>
+                    </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        usuarios: {
+            type: Array,
+            required: true,
+        }
+    },
+    filters: {
+        capitalize: function (value) {
+            if (!value) return ''
+                value = value.toString()
+                return value.charAt(0).toUpperCase() + value.slice(1)
+            }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
